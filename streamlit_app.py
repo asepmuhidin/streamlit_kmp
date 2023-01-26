31af93aca4e8833f80b11ace216be310be0b91f5
Asep Muhidin <asep.muhidin@gmail.com>
2023-01-26 11:02:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,9 @@
-from pathlib import Path
+import streamlit as st  
 
-import streamlit as st  # pip install streamlit
-from PIL import Image  # pip install pillow
+def main():
+    st.title("Welcome")
 
-# --- PATH SETTINGS ---
-THIS_DIR = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-ASSETS_DIR = THIS_DIR / "assets"
-STYLES_DIR = THIS_DIR / "styles"
-CSS_FILE = STYLES_DIR / "main.css"
-
-# --- GENERAL SETTINGS ---
-PRODUCT_NAME = "kmp"
-PRODUCT_TAGLINE = "kirata media perkasa"
+if __name__==__main__ :
+    main()
 
 
-
-def load_css_file(css_file_path):
-    with open(css_file_path) as f:
-        return st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-# --- PAGE CONFIG ---
-st.set_page_config(
-    page_title=PRODUCT_NAME,
-    page_icon=":u5272:",
-    layout="centered",
-    initial_sidebar_state="collapsed"
-)
-#load_css_file(CSS_FILE)
-
-st.head("Welcome")
