7f9d012144679fc75856f8d52a773a191a2faafb
Asep Muhidin <asep.muhidin@gmail.com>
2023-01-26 11:07:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st  
+from streamlit_option_menu import option_menu
+
+def sidebar():
+    with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
 
 def main():
     st.title("Welcome")
-
+    sidebar()
+    
 if __name__=='__main__' :
     main()
 
